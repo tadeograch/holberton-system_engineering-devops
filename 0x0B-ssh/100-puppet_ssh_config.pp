e74d3809988941886ebd0d473ee5d28d58d9ccf2
Tadeo Grach <tade.g.17@gmail.com>
2021-04-04 23:04:53 +0000

    adding advanced task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Connect to server without typing a password
+file_line { 'change private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+  match  => 'IdentityFile ~/.ssh/id_rsa',
+}
+file_line { 'no password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  match  => 'PasswordAuthentication yes',
+  line   => 'PasswordAuthentication no',
+}
